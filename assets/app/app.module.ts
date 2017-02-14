d35920b92c680f742df3e1f11ae8b18e5e30414b
Fareez Ahmed <fareez.ahmed@onedatascan.com>
2017-02-14 10:02:54 -0500

    lazy loading for auth module

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,37 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from "@angular/http";
+
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
-import { LogoutComponent } from "./auth/logout.component";
-import { SignupComponent } from "./auth/signup.component";
-import { SigninComponent } from "./auth/signin.component";
-import { ErrorComponent } from "./errors/error.component";
-import { routing } from './app.routing';
+import { HeaderComponent } from "./header.component";
+import { routing } from "./app.routing";
 import { AuthService } from "./auth/auth.service";
+import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
 import { MessageModule } from "./messages/message.module";
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthenticationComponent,
-    HeaderComponent,
-    LogoutComponent,
-    SignupComponent,
-    SigninComponent,
-    ErrorComponent
-  ],
-  imports: [
-    BrowserModule,
-    routing,
-    ReactiveFormsModule,
-    HttpModule,
-    MessageModule
-  ],
-  providers: [AuthService, ErrorService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthenticationComponent,
+        HeaderComponent,
+        ErrorComponent
+    ],
+    imports: [
+        BrowserModule,
+        routing,
+        HttpModule,
+        MessageModule
+    ],
+    providers: [AuthService, ErrorService],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 
